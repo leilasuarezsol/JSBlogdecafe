2f858b23b10f347fb90053f8d190dd91699f745e
leila <leili_suarez@hotmail.com>
2024-10-30 19:47:37 -0300

    mi primer commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const textoHeader = document.querySelector('#heading')
+
+console.log(textoHeader)
+
+textoHeader.textContent = "Café leila"
+
+textoHeader.classList.add('nueva-clasejs')
+
+const enlace = document.querySelectorAll('.navegacion a')
+
+console.log(enlaces[0])
+
+enlace[1].textContent = "nuevo enlace"
